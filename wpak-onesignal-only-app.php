b41d55f0e7505d01eebec0735ed3882151f72ee3
Mathieu Le Roi <mathleroi@gmail.com>
2018-09-25 15:10:35 +0200

    Use onesignal_send_notification filter

--- wpak-onesignal-only-app.php
+++ wpak-onesignal-only-app.php
@@ -7,4 +7,14 @@
   Version: 1.0.0
 */
 
-add_filter( 'do_send_notification', '__return_false' );
+add_filter( 'onesignal_send_notification', 'wpak_onesignal_only_app_bypass_default_push' );
+
+function wpak_onesignal_only_app_bypass_default_push( $fields ) {
+	$onesignal_wp_settings = OneSignal::get_onesignal_settings();
+	if ($onesignal_wp_settings['send_to_mobile_platforms'] == true) {
+		$fields['do_send_notification'] = false;
+	}
+	return $fields;
+}
+
+
